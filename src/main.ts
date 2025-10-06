23cd751c28f851f88c0d60ed0afd45db38ba93ff
Mohammad Hossein company <mh.vakiliwork@gmail.com>
2025-10-06 12:04:25 +0330

    feat: add cookie-parser for refresh token cookies

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
